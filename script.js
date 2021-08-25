2d388a3d773c94ccadbd6a86451194554f1badb9
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-26 00:13:15 +0300

    Third

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ for(let a = 0; a < arrS.length; a++) {
         console.log(arrS[a]);
     }
 }
+
+//Third
+console.log('Third');
+let arrTF = [];
+console.log('For');
+for(let b = 23; b <= 57; b++) {
+    arrTF.push(b);
+}
+console.log(arrTF);
+console.log('while')
+let arrTW = [];
+let c = 23;
+while(c <= 57){
+    arrTW.push(c);
+    c++;
+}
+console.log(arrTW);
+let resultT = 0;
+for(d = 0; d < arrTF.length; d++) {
+    resultT = resultT + arrTF[d];
+}
+console.log('Сумма чисел данного массива равна ' + resultT);
